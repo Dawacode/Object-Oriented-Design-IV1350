a4e85b8683d4e5e0b7a585d5882b7b57e88943e3
Dawacode <dawaarkhang@gmail.com>
2024-05-15 14:28:01 +0200

    fixed a itemException class

--- src/exceptions/DataBaseException.java
+++ src/exceptions/DataBaseException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class DataBaseException extends Exception {
     public DataBaseException(String message) {
-        super("The data base is not up and running");
+        super("The Inventory is not reachable");
     }
 }
