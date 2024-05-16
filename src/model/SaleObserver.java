fb062fc82536e1281f0e722b96945ffddff06db1
Dawacode <dawaarkhang@gmail.com>
2024-05-16 13:28:07 +0200

    comments are written for logger and saleObserver

--- src/model/SaleObserver.java
+++ src/model/SaleObserver.java
@@ -1,7 +1,14 @@
 package model;
 
+/**
+ * SaleObserver interface provides a method to be implemented by observers
+ * that need to be notified about changes in total revenue.
+ */
 public interface SaleObserver {
-    //void newReceipt(ReceiptDTO receipt);
-
+    /**
+     * This method is called to update the total revenue.
+     *
+     * @param totalRevenue The new total revenue to be updated.
+     */
     void updateTotalRevenue(int totalRevenue);
 }
